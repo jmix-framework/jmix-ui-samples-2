2317acbf975fa661623ae30ad98021649778d431
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-06-23 17:24:23 +0400

    Split samples added, menu width fixed

---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/containers/split/simple/SplitSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.flowuisampler.view.flowui.containers.split.simple;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("split-simple")
+@ViewDescriptor("split-simple.xml")
+public class SplitSimpleSample extends StandardView {
+}
---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/containers/split/themevariant/SplitThemeVariantSample.java
@@ -0,0 +1,42 @@
+package io.jmix.flowuisampler.view.flowui.containers.split.themevariant;
+
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import io.jmix.flowui.component.checkboxgroup.JmixCheckboxGroup;
+import io.jmix.flowui.component.splitlayout.JmixSplitLayout;
+import io.jmix.flowui.view.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@ViewController("split-theme-variant")
+@ViewDescriptor("split-theme-variant.xml")
+public class SplitThemeVariantSample extends StandardView {
+
+    @ViewComponent
+    protected JmixSplitLayout split;
+    @ViewComponent
+    protected JmixCheckboxGroup<String> settingsCheckboxGroup;
+
+    @Subscribe
+    protected void onInit(InitEvent event) {
+        List<String> settingsItems = List.of("Minimal", "Small", "Splitter-spacing");
+        settingsCheckboxGroup.setItems(settingsItems);
+    }
+
+    @Subscribe("settingsCheckboxGroup")
+    protected void onSettingsCheckboxGroupValueChange(ComponentValueChangeEvent<JmixCheckboxGroup<String>, Collection<String>> event) {
+        if (event.getValue() == null) {
+            return;
+        }
+
+        applySplitSettings(event.getValue());
+    }
+
+    protected void applySplitSettings(Collection<String> settings) {
+        split.getElement().getThemeList().clear();
+
+        settings.stream()
+                .map(String::toLowerCase)
+                .forEach(split::addThemeName);
+    }
+}
