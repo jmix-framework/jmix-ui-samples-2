a16307d82d3d90187d6b17107ab7333dda223afb
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-06-23 15:58:57 +0400

    FormLayout samples added

---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/containers/formlayout/responsivesteps/FormLayoutResponsiveStepsSample.java
@@ -0,0 +1,10 @@
+package io.jmix.flowuisampler.view.flowui.containers.formlayout.responsivesteps;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("form-layout-responsive-steps")
+@ViewDescriptor("form-layout-responsive-steps.xml")
+public class FormLayoutResponsiveStepsSample extends StandardView {
+}
---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/containers/formlayout/simple/FormLayoutSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.flowuisampler.view.flowui.containers.formlayout.simple;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("form-layout-simple")
+@ViewDescriptor("form-layout-simple.xml")
+public class FormLayoutSimpleSample extends StandardView {
+}
