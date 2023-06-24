1794531768561f0f33d10e05f07358b36143b2ef
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-06-24 11:08:45 +0400

    Update to Vaadin 24.0.8, TabSheet samples added

---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/containers/tabsheet/simple/TabSheetSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.flowuisampler.view.flowui.containers.tabsheet.simple;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("tabsheet-simple")
+@ViewDescriptor("tabsheet-simple.xml")
+public class TabSheetSimpleSample extends StandardView {
+}
