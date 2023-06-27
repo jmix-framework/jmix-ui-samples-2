f9f6f1fb9ac7ffb33f00d090645854a9ddec0203
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-06-27 18:05:18 +0400

    Select samples added

---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/components/select/simple/SelectSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.flowuisampler.view.flowui.components.select.simple;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("select-simple")
+@ViewDescriptor("select-simple.xml")
+public class SelectSimpleSample extends StandardView {
+}
