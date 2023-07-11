3c12de5327a2632c428fb24fdb00e55f114c583c
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-07-11 15:58:28 +0400

    DateTimePicker theme variant fix

--- src/main/java/io/jmix/flowuisampler/view/flowui/components/datetimepicker/themevariant/DateTimePickerThemeVariantSample.java
+++ src/main/java/io/jmix/flowuisampler/view/flowui/components/datetimepicker/themevariant/DateTimePickerThemeVariantSample.java
@@ -1,25 +1,10 @@
 package io.jmix.flowuisampler.view.flowui.components.datetimepicker.themevariant;
 
-import io.jmix.flowui.component.datetimepicker.TypedDateTimePicker;
-import io.jmix.flowui.view.*;
-
-import java.time.LocalDateTime;
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
 
 @ViewController("date-time-picker-theme-variant")
 @ViewDescriptor("date-time-picker-theme-variant.xml")
 public class DateTimePickerThemeVariantSample extends StandardView {
-
-    @ViewComponent
-    protected TypedDateTimePicker<LocalDateTime> dateTimePicker;
-    @ViewComponent
-    protected TypedDateTimePicker<LocalDateTime> dateTimePicker2;
-    @ViewComponent
-    protected TypedDateTimePicker<LocalDateTime> dateTimePicker3;
-
-    @Subscribe
-    protected void onInit(InitEvent event) {
-        dateTimePicker.setTypedValue(LocalDateTime.now());
-        dateTimePicker2.setTypedValue(LocalDateTime.now());
-        dateTimePicker3.setTypedValue(LocalDateTime.now());
-    }
 }
