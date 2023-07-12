bd6418a4984870ad1d6aab821684de16183eae22
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-07-12 17:51:51 +0400

    PropertyFilter samples added

---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/components/propertyfilter/customfield/PropertyFilterCustomFieldSample.java
@@ -0,0 +1,10 @@
+package io.jmix.flowuisampler.view.flowui.components.propertyfilter.customfield;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("property-filter-custom-field")
+@ViewDescriptor("property-filter-custom-field.xml")
+public class PropertyFilterCustomFieldSample extends StandardView {
+}
---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/components/propertyfilter/keyvalue/PropertyFilterKeyValueSample.java
@@ -0,0 +1,10 @@
+package io.jmix.flowuisampler.view.flowui.components.propertyfilter.keyvalue;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("property-filter-key-value")
+@ViewDescriptor("property-filter-key-value.xml")
+public class PropertyFilterKeyValueSample extends StandardView {
+}
