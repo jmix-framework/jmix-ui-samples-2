525f17ee58e1de11f86feae7152c12069ecf88d1
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-07-17 16:51:31 +0400

    ValuePicker samples added

---
+++ src/main/java/io/jmix/flowuisampler/view/flowui/components/valuepicker/simple/ValuePickerSimpleSample.java
@@ -0,0 +1,36 @@
+package io.jmix.flowuisampler.view.flowui.components.valuepicker.simple;
+
+import io.jmix.flowui.component.valuepicker.JmixValuePicker;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.kit.component.valuepicker.CustomValueSetEvent;
+import io.jmix.flowui.view.*;
+import org.apache.commons.lang3.RandomStringUtils;
+
+@ViewController("value-picker-simple")
+@ViewDescriptor("value-picker-simple.xml")
+public class ValuePickerSimpleSample extends StandardView {
+
+    @ViewComponent
+    protected JmixValuePicker<String> valuePicker1;
+    @ViewComponent
+    protected JmixValuePicker<String> valuePicker2;
+
+    @Subscribe("valuePicker1.generate")
+    protected void onValuePicker1GenerateActionPerformed(ActionPerformedEvent event) {
+        generateValue(valuePicker1);
+    }
+
+    @Subscribe("valuePicker2.generate")
+    protected void onValuePicker2GenerateActionPerformed(ActionPerformedEvent event) {
+        generateValue(valuePicker2);
+    }
+
+    @Subscribe("valuePicker2")
+    protected void onValuePicker2CustomValueChange(CustomValueSetEvent<JmixValuePicker<String>, String> event) {
+        event.getSource().setValue(event.getText());
+    }
+
+    protected void generateValue(JmixValuePicker<String> valuePicker) {
+        valuePicker.setValue(RandomStringUtils.randomAlphabetic(5, 10));
+    }
+}
