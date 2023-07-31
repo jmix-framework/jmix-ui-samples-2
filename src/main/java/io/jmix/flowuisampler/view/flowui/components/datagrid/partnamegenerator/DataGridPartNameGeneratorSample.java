5637877739b20457a879c71466e3aee731f7f892
Georgii Vlasov <honest.niceman@gmail.com>
2023-07-31 14:39:28 +0400

    Upgrade Jmix up to 2.0.1

--- src/main/java/io/jmix/flowuisampler/view/flowui/components/datagrid/partnamegenerator/DataGridPartNameGeneratorSample.java
+++ src/main/java/io/jmix/flowuisampler/view/flowui/components/datagrid/partnamegenerator/DataGridPartNameGeneratorSample.java
@@ -1,12 +1,10 @@
 package io.jmix.flowuisampler.view.flowui.components.datagrid.partnamegenerator;
 
-import com.vaadin.flow.component.grid.Grid;
 import io.jmix.core.Metadata;
 import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.view.*;
 import io.jmix.flowuisampler.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.Nullable;
 
 @ViewController("data-grid-part-name-generator")
 @ViewDescriptor("data-grid-part-name-generator.xml")
@@ -18,21 +16,11 @@ public class DataGridPartNameGeneratorSample extends StandardView {
     @Autowired
     protected Metadata metadata;
 
-    @Subscribe
-    protected void onInit(InitEvent event) {
-        Grid.Column<Customer> gradeColumn = customersDataGrid.getColumnByKey("grade");
-
-        if (gradeColumn != null) {
-            gradeColumn.setPartNameGenerator(this::gradePartNameGenerator);
-        }
-    }
-
-    @Nullable
-    protected String gradePartNameGenerator(Customer customer) {
+    @Install(to = "customersDataGrid.grade", subject = "partNameGenerator")
+    protected String gradePartNameGenerator(final Customer customer) {
         if (customer.getGrade() == null) {
             return null;
         }
-
         return switch (customer.getGrade()) {
             case STANDARD -> "standard-grade";
             case HIGH -> "high-grade";
