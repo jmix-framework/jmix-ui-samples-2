b89da1d029a6cd5bb559b0bcf35b1b088ca42f0b
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-09-20 16:38:59 +0400

    DataGrid header-filter sample

---
+++ src/main/java/io/jmix/uisamples/view/flowui/components/datagrid/headerfilter/DataGridHeaderFilterSample.java
@@ -0,0 +1,10 @@
+package io.jmix.uisamples.view.flowui.components.datagrid.headerfilter;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("data-grid-header-filter")
+@ViewDescriptor("data-grid-header-filter.xml")
+public class DataGridHeaderFilterSample extends StandardView {
+}
