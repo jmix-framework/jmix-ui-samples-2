a373b3110a04836abb148e3525344448403f73b5
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-09-21 09:53:51 +0400

    FlexLayout sample

---
+++ src/main/java/io/jmix/uisamples/view/flowui/containers/boxlayout/flexbox/FlexLayoutSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.uisamples.view.flowui.containers.boxlayout.flexbox;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("flex-layout-simple")
+@ViewDescriptor("flex-layout-simple.xml")
+public class FlexLayoutSimpleSample extends StandardView {
+}
