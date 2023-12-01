c51d902000a9770cc33c5134a3a406dfb967973f
Amir Chelly <amirchelly99@gmail.com>
2023-12-01 22:07:46 +0300

    Added a simple fileUploadField example

---
+++ src/main/java/io/jmix/uisamples/view/flowui/components/fileuploadfield/FileUploadField.java
@@ -0,0 +1,11 @@
+package io.jmix.uisamples.view.flowui.components.fileuploadfield;
+
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("file-upload-field")
+@ViewDescriptor("file-upload-field.xml")
+public class FileUploadField extends StandardView {
+}
