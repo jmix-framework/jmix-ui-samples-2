bea0449fe139349029678b63de7810c34082babe
Amir Chelly <bharghaz@student.21-school.ru>
2023-12-14 17:03:58 +0300

    Add fileStorageUploadField and fileUploadField Components to UI Samples (#16)
    Co-authored-by: Georgii Vlasov <honest.niceman@gmail.com>
    Co-authored-by: Konstantin Krivopustov <krivopustov@haulmont.com>

---
+++ src/main/java/io/jmix/uisamples/view/flowui/components/fileuploadfields/fileuploadfield/FileUploadFieldSample.java
@@ -0,0 +1,23 @@
+package io.jmix.uisamples.view.flowui.components.fileuploadfields.fileuploadfield;
+
+import com.vaadin.flow.component.notification.NotificationVariant;
+import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.upload.FileStorageUploadField;
+import io.jmix.flowui.kit.component.upload.event.FileUploadSucceededEvent;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ViewController("file-upload-field")
+@ViewDescriptor("file-upload-field.xml")
+public class FileUploadFieldSample extends StandardView {
+
+    @Autowired
+    private Notifications notifications;
+
+    @Subscribe("fileUploadField")
+    public void onFileUploadFieldFileUploadSucceeded(final FileUploadSucceededEvent<FileStorageUploadField> event) {
+        notifications.create("Your file %s has been uploaded successfully.".formatted(event.getFileName()))
+                .withThemeVariant(NotificationVariant.LUMO_PRIMARY)
+                .show();
+    }
+}
