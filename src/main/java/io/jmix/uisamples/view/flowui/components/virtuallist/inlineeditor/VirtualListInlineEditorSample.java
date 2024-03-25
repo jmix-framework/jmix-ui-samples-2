23342dd44802ee2e69f32cc628d6e4920be7e7fd
kartondev <d.cherkasov@haulmont.com>
2024-03-25 17:13:38 +0300

    fix PR issues on VirtualList inline editor

--- src/main/java/io/jmix/uisamples/view/flowui/components/virtuallist/inlineeditor/VirtualListInlineEditorSample.java
+++ src/main/java/io/jmix/uisamples/view/flowui/components/virtuallist/inlineeditor/VirtualListInlineEditorSample.java
@@ -1,13 +1,13 @@
 package io.jmix.uisamples.view.flowui.components.virtuallist.inlineeditor;
 
 
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.avatar.AvatarVariant;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -18,53 +18,40 @@ import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.server.StreamResource;
 import io.jmix.core.LoadContext;
 import io.jmix.core.Messages;
-import io.jmix.core.Resources;
 import io.jmix.flowui.DialogWindows;
-import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.model.CollectionContainer;
-import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.view.*;
+import io.jmix.uisamples.bean.FoodPlaceholderDataGenerator;
 import io.jmix.uisamples.entity.Food;
 import io.jmix.uisamples.view.entity.food.FoodDetailView;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 @ViewController("virtual-list-inline-editor")
 @ViewDescriptor("virtual-list-inline-editor.xml")
-public class VirtualListInlineEditor extends StandardView {
-    private static final String NOOP_DESCRIPTION = "";
+public class VirtualListInlineEditorSample extends StandardView {
 
-    @Autowired
-    private Resources resources;
     @Autowired
     private Messages messages;
     @Autowired
     private DialogWindows dialogWindows;
+    @Autowired
+    private FoodPlaceholderDataGenerator foodPlaceholderDataGenerator;
 
     @ViewComponent
     private DataContext dataContext;
     @ViewComponent
     private CollectionContainer<Food> foodDc;
-    @ViewComponent
-    private CollectionLoader<Food> foodDl;
-
-    @Subscribe
-    public void onBeforeShow(final BeforeShowEvent event) {
-        foodDl.load();
-    }
 
     @Supply(to = "foodList", subject = "renderer")
     private Renderer<Food> foodListRenderer() {
         return new ComponentRenderer<>(item -> {
             HorizontalLayout rootCardLayout = new HorizontalLayout();
             rootCardLayout.setMargin(true);
-            rootCardLayout.setSpacing(true);
 
             VerticalLayout infoLayout = new VerticalLayout();
             infoLayout.setMargin(false);
@@ -75,7 +62,7 @@ public class VirtualListInlineEditor extends StandardView {
             Avatar avatar = new Avatar();
             avatar.addThemeVariants(AvatarVariant.LUMO_XLARGE);
 
-            if(item.getIcon() != null && item.getIcon().length > 0) {
+            if (item.getIcon() != null && item.getIcon().length > 0) {
                 String iconName = "%s.png".formatted(item.getTitle());
                 avatar.setImageResource(new StreamResource(iconName, () -> new ByteArrayInputStream(item.getIcon())));
             }
@@ -85,7 +72,9 @@ public class VirtualListInlineEditor extends StandardView {
 
             HorizontalLayout itemDetailLayout = new HorizontalLayout();
             itemDetailLayout.add(new Text(item.getDescription()));
-            itemDetailLayout.add(new Html(messages.formatMessage(getClass(), "foodListItemDescription", item.getPrice())));
+            itemDetailLayout.add(new Html(
+                    messages.formatMessage(getClass(), "foodListItemDescription", item.getPrice()))
+            );
             itemDetailLayout.setPadding(false);
             itemDetailLayout.setMargin(false);
             itemDetailLayout.setAlignItems(FlexComponent.Alignment.CENTER);
@@ -104,14 +93,11 @@ public class VirtualListInlineEditor extends StandardView {
             detailButton.addClickListener(e -> dialogWindows.detail(this, Food.class)
                     .withViewClass(FoodDetailView.class)
                     .editEntity(item)
-                    .withParentDataContext(getViewData().getDataContext())
                     .withAfterCloseListener(closeEvent -> {
-                        FoodDetailView detail = closeEvent.getSource().getView();
                         if (closeEvent.closedWith(StandardOutcome.SAVE)) {
-                            foodDc.replaceItem(detail.getEditedEntity());
+                            foodDc.replaceItem(closeEvent.getSource().getView().getEditedEntity());
                         }
                     })
-                    .build()
                     .open());
             detailButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
 
@@ -123,73 +109,27 @@ public class VirtualListInlineEditor extends StandardView {
                 dataContext.remove(dataContext.merge(item));
             });
 
-
             buttonsPanel.add(detailButton, removeButton);
             rootCardLayout.add(avatar, infoLayout, buttonsPanel);
             return rootCardLayout;
         });
-
     }
 
     @Install(to = "foodDl", target = Target.DATA_LOADER)
     private List<Food> foodDlLoadDelegate(final LoadContext<Food> loadContext) {
-        return getInitialFoodList();
+        return foodPlaceholderDataGenerator.getFoodSamplesList();
     }
 
-    private Food createFoodSample(String title,
-                                  String description,
-                                  int price,
-                                  String iconLocation) {
-
-        Food food = dataContext.create(Food.class);
-        food.setTitle(title);
-        food.setDescription(description);
-        food.setPrice(price);
-        try (InputStream imageStream = resources.getResourceAsStream(iconLocation)) {
-            food.setIcon(imageStream.readAllBytes());
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot open a stream", e);
-        }
-        return food;
-    }
-
-    private List<Food> getInitialFoodList() {
-        List<Food> foodList = new ArrayList<Food>();
-        foodList.add(createFoodSample(messages.getMessage(getClass(), "sample.burger.title"),
-                messages.getMessage(getClass(), "sample.burger.description"),
-                7, "META-INF/resources/icons/components/virtuallist-food/burger.png"));
-
-        foodList.add(createFoodSample(messages.getMessage(getClass(), "sample.cola.title"),
-                messages.getMessage(getClass(), "sample.cola.description"),
-                4, "META-INF/resources/icons/components/virtuallist-food/cola.png"));
-
-        foodList.add(createFoodSample(messages.getMessage(getClass(), "sample.fries.title"),
-                messages.getMessage(getClass(), "sample.fries.description"),
-                3, "META-INF/resources/icons/components/virtuallist-food/fries.png"));
-
-        foodList.add(createFoodSample(messages.getMessage(getClass(), "sample.pizza.title"),
-                messages.getMessage(getClass(), "sample.pizza.description"),
-                15, "META-INF/resources/icons/components/virtuallist-food/pizza.png"));
-
-        foodList.add(createFoodSample(messages.getMessage(getClass(), "sample.sandwich.title"),
-                NOOP_DESCRIPTION,
-                2, "META-INF/resources/icons/components/virtuallist-food/sandwich.png"));
-        return foodList;
-    }
-
-    @Subscribe("addAction")
-    public void onAddAction(final ActionPerformedEvent event) {
+    @Subscribe(id = "addBtn", subject = "clickListener")
+    public void onAddBtnClick(final ClickEvent<JmixButton> event) {
         dialogWindows.detail(this, Food.class)
                 .withViewClass(FoodDetailView.class)
                 .newEntity()
-                .withParentDataContext(getViewData().getDataContext())
                 .withAfterCloseListener(closeEvent -> {
-                    FoodDetailView detail = closeEvent.getSource().getView();
                     if (closeEvent.closedWith(StandardOutcome.SAVE)) {
-                        foodDc.replaceItem(detail.getEditedEntity());
+                        foodDc.replaceItem(closeEvent.getSource().getView().getEditedEntity());
                     }
                 })
-                .build()
                 .open();
     }
 }
