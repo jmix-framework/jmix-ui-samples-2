e33ce54d5d1c0afc6bd153f3a12b25d50085e2a4
Dmitry Kremnev <kremnev.dima@mail.ru>
2024-05-06 10:44:50 +0400

    Upgrade to Jmix 2.2.2

--- src/main/java/io/jmix/uisamples/view/flowui/components/datagrid/inlineeditor/DataGridInlineEditorSample.java
+++ src/main/java/io/jmix/uisamples/view/flowui/components/datagrid/inlineeditor/DataGridInlineEditorSample.java
@@ -1,44 +1,10 @@
 package io.jmix.uisamples.view.flowui.components.datagrid.inlineeditor;
 
-import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
-import io.jmix.core.DataManager;
-import io.jmix.flowui.Notifications;
-import io.jmix.flowui.component.checkbox.JmixCheckbox;
-import io.jmix.flowui.component.grid.DataGrid;
-import io.jmix.flowui.view.*;
-import io.jmix.uisamples.entity.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
 
 @ViewController("data-grid-inline-editor")
 @ViewDescriptor("data-grid-inline-editor.xml")
 public class DataGridInlineEditorSample extends StandardView {
-
-    @ViewComponent
-    protected DataGrid<Customer> customersDataGrid;
-
-    @Autowired
-    protected Notifications notifications;
-    @Autowired
-    protected DataManager dataManager;
-
-    @Subscribe("editorBufferedCheckbox")
-    protected void onEditorBufferedCheckboxValueChange(ComponentValueChangeEvent<JmixCheckbox, Boolean> event) {
-        if (!event.isFromClient()) {
-            return;
-        }
-
-        if (customersDataGrid.getEditor().isOpen()) {
-            notifications.create("Please close Editor before changing its mode")
-                    .withType(Notifications.Type.WARNING)
-                    .withCloseable(false)
-                    .show();
-
-            event.getSource().setValue(event.getOldValue());
-        } else {
-            customersDataGrid.getEditor().setBuffered(event.getValue());
-
-            customersDataGrid.getColumnByKey("bufferedEditorColumn").setVisible(event.getValue());
-            customersDataGrid.getColumnByKey("nonBufferedEditorColumn").setVisible(!event.getValue());
-        }
-    }
 }
