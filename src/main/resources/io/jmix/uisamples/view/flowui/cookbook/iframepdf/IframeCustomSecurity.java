8e5493a50a287d4b32d1db57ee4995a8e2140a14
Dmitry Kremnev <kremnev.dima@mail.ru>
2024-07-01 13:12:57 +0400

    Update UI Samples to 2.3 jmix-framework/jmix-ui-samples-3#35

--- src/main/resources/io/jmix/uisamples/view/flowui/cookbook/iframepdf/IframeCustomSecurity.java
+++ src/main/resources/io/jmix/uisamples/view/flowui/cookbook/iframepdf/IframeCustomSecurity.java
@@ -1,11 +1,11 @@
 package io.jmix.uisamples.view.flowui.cookbook.iframepdf;
 
-import io.jmix.securityflowui.FlowuiSecurityConfiguration;
+import io.jmix.securityflowui.security.FlowuiVaadinWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
-public class IframeCustomSecurity extends FlowuiSecurityConfiguration {
+public class IframeCustomSecurity extends FlowuiVaadinWebSecurity {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
