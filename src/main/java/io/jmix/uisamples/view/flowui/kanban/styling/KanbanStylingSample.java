d337c12cf7432ed921f5ba6b4183cd21d3138a84
Dmitry Kremnev <kremnev.dima@mail.ru>
2024-08-05 13:18:08 +0400

    Kanban samples [part 3]

---
+++ src/main/java/io/jmix/uisamples/view/flowui/kanban/styling/KanbanStylingSample.java
@@ -0,0 +1,10 @@
+package io.jmix.uisamples.view.flowui.kanban.styling;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController("kanban-styling")
+@ViewDescriptor("kanban-styling.xml")
+public class KanbanStylingSample extends StandardView {
+}
