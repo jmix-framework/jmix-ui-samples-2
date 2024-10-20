883221a658e1f8e0e4881132d6a9ba4083290a95
Roman Pinyazhin <pinyazhin@haulmont.com>
2024-10-20 18:46:55 +0400

    Calendar samples (WiP)

---
+++ src/main/java/io/jmix/uisamples/view/flowui/calendar/containercalendardataprovider/CalendarContainerCalendarDataProvider.java
@@ -0,0 +1,10 @@
+package io.jmix.uisamples.view.flowui.calendar.containercalendardataprovider;
+
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@ViewController(id = "calendar-container-calendar-data-provider")
+@ViewDescriptor(path = "calendar-container-calendar-data-provider.xml")
+public class CalendarContainerCalendarDataProvider extends StandardView {
+}
