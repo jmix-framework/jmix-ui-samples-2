4f99282ad68c8cff9c76e32814bc0c03e05e4355
Konstantin Krivopustov <krivopustov@haulmont.com>
2025-03-12 18:53:15 +0400

    In 2-level composition sample, MeetingPoint is saved only when other Terminal attribute is changed #50

--- src/main/java/io/jmix/uisamples/view/flowui/cookbook/composition2/TerminalDetailView.java
+++ src/main/java/io/jmix/uisamples/view/flowui/cookbook/composition2/TerminalDetailView.java
@@ -1,13 +1,24 @@
 package io.jmix.uisamples.view.flowui.cookbook.composition2;
 
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.model.DataContext;
+import io.jmix.flowui.model.InstanceContainer;
+import io.jmix.flowui.view.*;
+import io.jmix.uisamples.entity.MeetingPoint;
 import io.jmix.uisamples.entity.Terminal;
 
 @ViewController(id = "terminal-detail-2")
 @ViewDescriptor(path = "terminal-detail-view.xml")
 @EditedEntityContainer("terminalDc")
 public class TerminalDetailView extends StandardDetailView<Terminal> {
+
+    @ViewComponent
+    private DataContext dataContext;
+
+    @Subscribe(id = "meetingPointsDc", target = Target.DATA_CONTAINER)
+    public void onMeetingPointsDcItemPropertyChange(final InstanceContainer.ItemPropertyChangeEvent<MeetingPoint> event) {
+        // Mark the root edited `Terminal` entity as modified in `DataContext`
+        // when a meeting point's property changes
+        Terminal terminal = getEditedEntity();
+        dataContext.setModified(terminal, true);
+    }
 }
