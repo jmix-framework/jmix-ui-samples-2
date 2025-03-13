9c54eb10cc209c2126208b884a979811a9986c2b
Dmitry Kremnev <kremnev.dima@mail.ru>
2025-03-13 12:54:27 +0400

    Update copy to clipboard sample

--- src/main/java/io/jmix/uisamples/view/flowui/cookbook/copytoclipboard/CopyToClipboardSample.java
+++ src/main/java/io/jmix/uisamples/view/flowui/cookbook/copytoclipboard/CopyToClipboardSample.java
@@ -4,8 +4,8 @@ import com.google.common.base.Strings;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
-import com.vaadin.flow.dom.Element;
 import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.UiComponentUtils;
 import io.jmix.flowui.component.textfield.TypedTextField;
 import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
@@ -23,10 +23,9 @@ public class CopyToClipboardSample extends StandardView {
 
     @Subscribe("copyButton")
     public void onCopyButtonClick(ClickEvent<JmixButton> event) {
-        Element buttonElement = event.getSource().getElement();
         String valueToCopy = Strings.nullToEmpty(textField.getTypedValue());
 
-        buttonElement.executeJs(getCopyToClipboardScript(), valueToCopy)
+        UiComponentUtils.copyToClipboard(valueToCopy)
                 .then(successResult -> notifications.create("Text copied!")
                                 .withPosition(Notification.Position.BOTTOM_END)
                                 .withThemeVariant(NotificationVariant.LUMO_SUCCESS)
@@ -35,20 +34,5 @@ public class CopyToClipboardSample extends StandardView {
                                 .withPosition(Notification.Position.BOTTOM_END)
                                 .withThemeVariant(NotificationVariant.LUMO_ERROR)
                                 .show());
-    }
-
-    protected String getCopyToClipboardScript() {
-        return """
-                  const textarea = document.createElement("textarea");
-                  textarea.value = $0;
-                  
-                  textarea.style.position = "absolute";
-                  textarea.style.opacity = "0";
-                  
-                  document.body.appendChild(textarea);
-                  textarea.select();
-                  document.execCommand("copy");
-                  document.body.removeChild(textarea);
-                """;
     }
 }
