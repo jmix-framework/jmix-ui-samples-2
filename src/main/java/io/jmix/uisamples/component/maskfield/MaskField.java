1e77f036e8eaa4080005ac6e11bce4bbbd09e245
Dmitry Kremnev <kremnev.dima@mail.ru>
2025-06-11 11:30:29 +0400

    Code cleanup

--- src/main/java/io/jmix/uisamples/component/maskfield/MaskField.java
+++ src/main/java/io/jmix/uisamples/component/maskfield/MaskField.java
@@ -2,7 +2,10 @@ package io.jmix.uisamples.component.maskfield;
 
 import com.vaadin.componentfactory.addons.inputmask.InputMask;
 import com.vaadin.componentfactory.addons.inputmask.InputMaskOption;
-import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.HasHelper;
+import com.vaadin.flow.component.HasLabel;
+import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.shared.HasPrefix;
 import com.vaadin.flow.component.shared.HasSuffix;
 import io.jmix.flowui.UiComponents;
@@ -14,7 +17,6 @@ import org.springframework.context.ApplicationContextAware;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 public class MaskField extends Composite<TypedTextField<String>>
         implements ApplicationContextAware, HasLabel, HasSize, HasHelper, HasSuffix, HasPrefix {
@@ -69,182 +71,12 @@ public class MaskField extends Composite<TypedTextField<String>>
         setOptions(options);
     }
 
-    @Override
-    public String getLabel() {
-        return getContent().getLabel();
-    }
-
-    @Override
-    public void setLabel(String label) {
-        getContent().setLabel(label);
-    }
-
     public String getValue() {
         return root.getTypedValue();
     }
 
     public void setValue(String value) {
         root.setTypedValue(value);
-    }
-
-    @Override
-    public void setWidth(String width) {
-        root.setWidth(width);
-    }
-
-    @Override
-    public void setWidth(float width, Unit unit) {
-        root.setWidth(width, unit);
-    }
-
-    @Override
-    public void setMinWidth(String minWidth) {
-        root.setMinWidth(minWidth);
-    }
-
-    @Override
-    public void setMinWidth(float minWidth, Unit unit) {
-        root.setMinWidth(minWidth, unit);
-    }
-
-    @Override
-    public void setMaxWidth(String maxWidth) {
-        root.setMaxWidth(maxWidth);
-    }
-
-    @Override
-    public void setMaxWidth(float maxWidth, Unit unit) {
-        root.setMaxWidth(maxWidth, unit);
-    }
-
-    @Override
-    public String getWidth() {
-        return root.getWidth();
-    }
-
-    @Override
-    public String getMinWidth() {
-        return root.getMinWidth();
-    }
-
-    @Override
-    public String getMaxWidth() {
-        return root.getMaxWidth();
-    }
-
-    @Override
-    public Optional<Unit> getWidthUnit() {
-        return root.getWidthUnit();
-    }
-
-    @Override
-    public void setHeight(String height) {
-        root.setHeight(height);
-    }
-
-    @Override
-    public void setHeight(float height, Unit unit) {
-        root.setHeight(height, unit);
-    }
-
-    @Override
-    public void setMinHeight(String minHeight) {
-        root.setMinHeight(minHeight);
-    }
-
-    @Override
-    public void setMinHeight(float minHeight, Unit unit) {
-        root.setMinHeight(minHeight, unit);
-    }
-
-    @Override
-    public void setMaxHeight(String maxHeight) {
-        root.setMaxHeight(maxHeight);
-    }
-
-    @Override
-    public void setMaxHeight(float maxHeight, Unit unit) {
-        root.setMaxHeight(maxHeight, unit);
-    }
-
-    @Override
-    public String getHeight() {
-        return root.getHeight();
-    }
-
-    @Override
-    public String getMinHeight() {
-        return root.getMinHeight();
-    }
-
-    @Override
-    public String getMaxHeight() {
-        return root.getMaxHeight();
-    }
-
-    @Override
-    public Optional<Unit> getHeightUnit() {
-        return root.getHeightUnit();
-    }
-
-    @Override
-    public void setSizeFull() {
-        root.setSizeFull();
-    }
-
-    @Override
-    public void setWidthFull() {
-        root.setWidthFull();
-    }
-
-    @Override
-    public void setHeightFull() {
-        root.setHeightFull();
-    }
-
-    @Override
-    public void setSizeUndefined() {
-        root.setSizeUndefined();
-    }
-
-    @Override
-    public String getHelperText() {
-        return root.getHelperText();
-    }
-
-    @Override
-    public void setHelperText(String helperText) {
-        root.setHelperText(helperText);
-    }
-
-    @Override
-    public void setHelperComponent(Component component) {
-        root.setHelperComponent(component);
-    }
-
-    @Override
-    public Component getHelperComponent() {
-        return root.getHelperComponent();
-    }
-
-    @Override
-    public void setPrefixComponent(Component component) {
-        root.setPrefixComponent(component);
-    }
-
-    @Override
-    public Component getPrefixComponent() {
-        return root.getPrefixComponent();
-    }
-
-    @Override
-    public void setSuffixComponent(Component component) {
-        root.setSuffixComponent(component);
-    }
-
-    @Override
-    public Component getSuffixComponent() {
-        return root.getSuffixComponent();
     }
 
     private UiComponents uiComponents() {
